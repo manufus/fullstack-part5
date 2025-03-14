db16d4bd5e029be2d5b23853c842044308a959ba
Manuel Hortelano Rodríguez <manuel.hortelano.rodriguez@gmail.com>
2025-03-14 19:56:05 +0100

    exercise 5.14

--- frontend/src/components/Blog.test.jsx
+++ frontend/src/components/Blog.test.jsx
@@ -2,6 +2,7 @@ import { render, screen } from '@testing-library/react'
 import '@testing-library/jest-dom'
 import Blog from './Blog'
 import { expect } from 'vitest'
+import userEvent from '@testing-library/user-event'
 
 test('renders blogs title and author', () => {
   const blog = {
@@ -20,4 +21,26 @@ test('renders blogs title and author', () => {
   expect(div).toHaveTextContent('THE GOAT')
   expect(div).not.toHaveTextContent('madrid.com')
   expect(div).not.toHaveTextContent('33')
+})
+
+test('URL and likes shown after clicking', async () => {
+  const blog = {
+    author: 'CR7',
+    title: 'THE GOAT',
+    url: 'madrid.com',
+    likes: 33,
+    user: {
+      username: 'tumadre',
+    },
+  }
+
+  const { container } = render(<Blog blog={blog} />)
+  const user = userEvent.setup()
+  const button = screen.getByText('view')
+  await user.click(button)
+
+  const div = container.querySelector('.blog')
+
+  expect(div).toHaveTextContent('madrid.com')
+  expect(div).toHaveTextContent('33')
 })
